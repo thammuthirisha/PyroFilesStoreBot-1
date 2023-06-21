4b36e4365b81d52fc1d319e0a32ebdaf470845e6
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-06-21 15:47:56 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -92,7 +92,7 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
         file_name = media_type.file_name
         await editable.edit(
             "**Your File Stored in my Database!**\n\n"
-            f"{file_name}"
+            f"**{file_name}**\n\n"
             f"Here is the Permanent Link of your file: {share_link} \n\n"
             "Just Click the link to get your file!",
             reply_markup=InlineKeyboardMarkup(
