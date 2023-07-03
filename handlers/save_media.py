d49d222602b741ec63aada250ec8d40adf4ff45a
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-07-03 15:44:06 +0530

    Update save_media.py

--- handlers/save_media.py
+++ handlers/save_media.py
@@ -88,7 +88,9 @@ async def save_media_in_channel(bot: Client, editable: Message, message: Message
             f"#PRIVATE_FILE:\n\n[{message.from_user.first_name}](tg://user?id={message.from_user.id}) Got File Link!",
             disable_web_page_preview=True)
         share_link = f"https://t.me/{Config.BOT_USERNAME}?start=ThammuTV_{str_to_b64(file_er_id)}"
-       caption = message.caption if media.file_name else""
+       media_type = message.document or message.video or message.audio 
+        
+caption = media_type.caption
         await editable.edit(
             
             f"**{caption}**\n\n"
